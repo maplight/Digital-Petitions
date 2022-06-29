6e78737b49a61cd996652afd9e6af4d08e9fb76e
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-06-29 17:05:31 -0400

    DP-37-add side menu to layout

---
+++ src/app/city-staff/city-staff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-city-staff',
+  templateUrl: './city-staff.component.html',
+  styleUrls: ['./city-staff.component.scss']
+})
+export class CityStaffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { HeaderModule } from './header/header.module';
 import { RouterModule } from '@angular/router';
+import { LayoutMaterialModule } from './layout-material.module';
+import { SideMenuModule } from 'src/app/core/layout/side-menu/side-menu.module';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, HeaderModule, RouterModule],
+  imports: [
+    CommonModule,
+    HeaderModule,
+    RouterModule,
+    LayoutMaterialModule,
+    SideMenuModule,
+  ],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
