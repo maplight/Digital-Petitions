92a2de191ffdef91a40974e7017ed36952be550a
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-06-29 11:05:30 -0400

    DP-35-refactorize code

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
   constructor() {}
