2fdcd42db6ec20b5f316342feb9adb218bd3c059
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-06-28 20:49:02 -0400

    DP-35 Implement a simple layout with header and no side menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: 'example',
-    loadChildren: () =>
-      import('./example/example.module').then((m) => m.ExampleModule),
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'example',
+        loadChildren: () =>
+          import('./example/example.module').then((m) => m.ExampleModule),
+      },
+    ],
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ThemingService } from './core/dynamic-theme/theming.service';
+import { LayoutModule } from './core/layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+  ],
   providers: [
     {
       provide: APP_INITIALIZER,
