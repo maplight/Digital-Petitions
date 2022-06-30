80940a84c8f6d53ea04d30e0d0de34e78fb7d325
lunox94 <gmrlunox94@gmail.com>
2022-06-30 00:24:06 -0400

    DP-37 remove side menu material module

--- src/app/core/layout/side-menu/side-menu.module.ts
+++ src/app/core/layout/side-menu/side-menu.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from './side-menu.component';
-import { SideMenuMaterialModule } from './side-menu-material.module';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [SideMenuComponent],
-  imports: [CommonModule, SideMenuMaterialModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    //material imports
+    MatIconModule,
+    MatListModule,
+  ],
   exports: [SideMenuComponent],
 })
 export class SideMenuModule {}
