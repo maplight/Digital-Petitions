a396d3e61514eff3adea72f7e5cced502cc7ee69
lunox94 <gmrlunox94@gmail.com>
2022-06-30 00:19:23 -0400

    DP-37 show selected indicator on menu

--- src/app/core/layout/side-menu/side-menu.component.ts
+++ src/app/core/layout/side-menu/side-menu.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'dp-side-menu',
   templateUrl: './side-menu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SideMenuComponent implements OnInit {
-  items = [
+  readonly items = [
     {
       icon: 'home',
-      route: 'city-staff/home',
+      route: '/city-staff/home',
       text: 'Home',
     },
     {
       icon: 'settings_applications',
-      route: 'city-staff/admin',
+      route: '/city-staff/admin',
       text: 'Site Admin',
     },
   ];
-  constructor() {}
 
-  ngOnInit(): void {}
+  protected activatedRoute$!: Observable<string>;
+
+  constructor(private _router: Router) {}
+
+  ngOnInit(): void {
+    this.activatedRoute$ = this._router.events.pipe(
+      startWith(null),
+      filter((event) => event === null || event instanceof NavigationEnd),
+      map((_event) => this._router.url)
+    );
+  }
 }
