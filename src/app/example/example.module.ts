743a64d331e6a8cd5026841737ef131401783db8
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-01 10:21:35 -0400

    DP-41-add support for icons

--- src/app/example/example.module.ts
+++ src/app/example/example.module.ts
@@ -5,8 +5,24 @@ import { ExampleRoutingModule } from './example-routing.module';
 import { ExampleComponent } from './example.component';
 import { ExampleMaterialModule } from './example-material.module';
 
+import { menu, annotation, HeroIconModule } from 'ng-heroicon';
+
 @NgModule({
   declarations: [ExampleComponent],
-  imports: [CommonModule, ExampleRoutingModule, ExampleMaterialModule],
+  imports: [
+    CommonModule,
+    ExampleRoutingModule,
+    ExampleMaterialModule,
+    HeroIconModule.forRoot(
+      {
+        menu,
+        annotation,
+      },
+      {
+        defaultHostDisplay: 'inlineBlock', // default 'none'
+        attachDefaultDimensionsIfNoneFound: true, // default 'false'
+      }
+    ),
+  ],
 })
 export class ExampleModule {}
