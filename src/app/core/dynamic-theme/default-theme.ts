f48e0653833ce51cfe3d283655aeed6da8c68f4c
lunox94 <gmrlunox94@gmail.com>
2022-07-01 22:34:52 -0400

    DP-40 set secondary button styles

--- src/app/core/dynamic-theme/default-theme.ts
+++ src/app/core/dynamic-theme/default-theme.ts
@@ -3,7 +3,7 @@ import { ThemeConfig } from './theme-config';
 export const DEFAULT_THEME: ThemeConfig = {
   themeId: 'default-theme-id',
   mainColors: {
-    primaryColor: '#0c3edf',
+    primaryColor: '#2D5BFF',
     accentColor: '#46a35e',
     warnColor: '#ff0700',
     headerColor: '#FFFFFF',
