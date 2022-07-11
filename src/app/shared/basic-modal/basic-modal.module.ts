a956a8bcc176e607cf762ecaf0f04184c4bbb8f3
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-11 19:24:14 -0400

    DP-78-Add the styles and logic of the basic dialog

--- src/app/shared/basic-modal/basic-modal.module.ts
+++ src/app/shared/basic-modal/basic-modal.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicModalComponent } from './basic-modal.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [BasicModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatDialogModule, MatButtonModule],
   exports: [BasicModalComponent],
 })
 export class BasicModalModule {}
