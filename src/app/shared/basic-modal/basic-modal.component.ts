0e35dacea19e28ec3c91148d43bf0a6298ac9b67
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-11 16:29:50 -0400

    DP-78-Create a basic template for the modal

---
+++ src/app/shared/basic-modal/basic-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-basic-modal',
+  templateUrl: './basic-modal.component.html',
+  styleUrls: ['./basic-modal.component.scss']
+})
+export class BasicModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
