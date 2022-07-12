50945aab472a26bad47affdb1109e9a186f7d9a4
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-12 17:54:59 -0400

    DP-73-Create the email change component

---
+++ src/app/auth/email-change-modal/email-change-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-email-change-modal',
+  templateUrl: './email-change-modal.component.html',
+  styleUrls: ['./email-change-modal.component.scss']
+})
+export class EmailChangeModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/auth/email-change-modal/email-change-modal.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmailChangeModalComponent } from './email-change-modal.component';
+
+
+
+@NgModule({
+  declarations: [
+    EmailChangeModalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmailChangeModalModule { }
