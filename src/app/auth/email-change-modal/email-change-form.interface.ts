3316f5d0e298e47211e5dd718fc1fe5f3c699343
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-12 18:12:02 -0400

    DP-73-Add the logic to the email change modal

---
+++ src/app/auth/email-change-modal/email-change-form.interface.ts
@@ -0,0 +1,5 @@
+import { FormControl } from '@angular/forms';
+
+export interface EmailChangeForm {
+  email: FormControl;
+}
--- src/app/auth/email-change-modal/email-change-modal.module.ts
+++ src/app/auth/email-change-modal/email-change-modal.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailChangeModalComponent } from './email-change-modal.component';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BasicModalModule } from 'src/app/shared/basic-modal/basic-modal.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ConfirmEmailChangeModalModule } from '../confirm-email-change-modal/confirm-email-change-modal.module';
 
 @NgModule({
-  declarations: [
-    EmailChangeModalComponent
-  ],
+  declarations: [EmailChangeModalComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ConfirmEmailChangeModalModule,
+    BasicModalModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class EmailChangeModalModule { }
+export class EmailChangeModalModule {}
