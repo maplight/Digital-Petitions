3beb7e406234382ce3c43160ee442e39957770d2
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-12 16:59:47 -0400

    DP-75-Add the logic to the email confirmation modal

--- src/app/auth/confirm-email-change-modal/confirm-email-change-modal.component.ts
+++ src/app/auth/confirm-email-change-modal/confirm-email-change-modal.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { DialogResultComponent } from 'src/app/shared/dialog-result/dialog-result.component';
+import { ConfirmEmailChangeForm } from './confirm-email-change-form.interface';
 
 @Component({
   selector: 'dp-confirm-email-change-modal',
   templateUrl: './confirm-email-change-modal.component.html',
-  styleUrls: ['./confirm-email-change-modal.component.scss']
+  styleUrls: ['./confirm-email-change-modal.component.scss'],
 })
 export class ConfirmEmailChangeModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public formGroup: FormGroup;
+  public form_data: ConfirmEmailChangeForm = {
+    code: new FormControl('', [Validators.required]),
+  };
+  constructor(
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<ConfirmEmailChangeModalComponent>,
+    public dialog: MatDialog
+  ) {
+    this.formGroup = this.formBuilder.group(this.form_data);
   }
 
+  ngOnInit(): void {}
+
+  onOk() {
+    if (this.formGroup.valid) {
+      console.log(this.formGroup.value);
+      this.dialogRef.close();
+      this.openDialog('Title', 'This is a long example text', true);
+    } else {
+      this.formGroup.markAllAsTouched();
+    }
+  }
+
+  openDialog(title: string, message: string, success: boolean): void {
+    const dialogRef = this.dialog.open(DialogResultComponent, {
+      width: '520px',
+      data: {
+        title: title,
+        message: message,
+        success: success,
+      },
+    });
+  }
 }
--- src/app/auth/confirm-email-change-modal/confirm-email-change-modal.module.ts
+++ src/app/auth/confirm-email-change-modal/confirm-email-change-modal.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmEmailChangeModalComponent } from './confirm-email-change-modal.component';
-
-
+import { DialogResultModule } from 'src/app/shared/dialog-result/dialog-result.module';
+import { BasicModalModule } from 'src/app/shared/basic-modal/basic-modal.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    ConfirmEmailChangeModalComponent
-  ],
+  declarations: [ConfirmEmailChangeModalComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    DialogResultModule,
+    BasicModalModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class ConfirmEmailChangeModalModule { }
+export class ConfirmEmailChangeModalModule {}
