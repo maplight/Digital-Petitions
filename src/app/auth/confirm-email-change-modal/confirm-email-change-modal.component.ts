d8ba7d6c28b1ba60cd7a854e06d3d855c44d0b94
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-12 15:58:23 -0400

    DP-75-Create the email change confirmation component

---
+++ src/app/auth/confirm-email-change-modal/confirm-email-change-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-confirm-email-change-modal',
+  templateUrl: './confirm-email-change-modal.component.html',
+  styleUrls: ['./confirm-email-change-modal.component.scss']
+})
+export class ConfirmEmailChangeModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/auth/confirm-email-change-modal/confirm-email-change-modal.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfirmEmailChangeModalComponent } from './confirm-email-change-modal.component';
+
+
+
+@NgModule({
+  declarations: [
+    ConfirmEmailChangeModalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConfirmEmailChangeModalModule { }
