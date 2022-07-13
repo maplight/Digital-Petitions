f4d87b66225c1f436f644f24dd74fd1d4a299abe
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-13 16:56:09 -0400

    DP-72-Create module and account settings component

---
+++ src/app/features/committee-account-settings/committee-account-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-committee-account-settings',
+  templateUrl: './committee-account-settings.component.html',
+  styleUrls: ['./committee-account-settings.component.scss']
+})
+export class CommitteeAccountSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/features/committee-account-settings/committee-account-settings.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommitteeAccountSettingsComponent } from './committee-account-settings.component';
+
+
+
+@NgModule({
+  declarations: [
+    CommitteeAccountSettingsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommitteeAccountSettingsModule { }
