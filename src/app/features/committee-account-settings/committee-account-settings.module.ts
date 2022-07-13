91e499155e2894ec2bcf73536d8f1624913e160d
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-13 17:14:32 -0400

    DP-72-Configure the path to account settings

--- src/app/features/committee-account-settings/committee-account-settings.module.ts
+++ src/app/features/committee-account-settings/committee-account-settings.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommitteeAccountSettingsComponent } from './committee-account-settings.component';
-
-
+import { CommitteeAccountSettingsRoutingModule } from './committee-account-settings-routing.module';
 
 @NgModule({
-  declarations: [
-    CommitteeAccountSettingsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CommitteeAccountSettingsComponent],
+  imports: [CommonModule, CommitteeAccountSettingsRoutingModule],
 })
-export class CommitteeAccountSettingsModule { }
+export class CommitteeAccountSettingsModule {}
