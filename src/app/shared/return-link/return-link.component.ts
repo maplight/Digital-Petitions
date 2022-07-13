ebf972e5d5529e926dad439db66725d4a4497fd0
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-13 18:09:00 -0400

    DP-72-Create the shared component return-link

---
+++ src/app/shared/return-link/return-link.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-return-link',
+  templateUrl: './return-link.component.html',
+})
+export class ReturnLinkComponent implements OnInit {
+  @Input()
+  route: string = '#';
+
+  @Input()
+  text: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
