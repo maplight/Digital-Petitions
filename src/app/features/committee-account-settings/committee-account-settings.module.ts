4d7832c5bcbda87225fe3b79607ce6f6ff892024
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-13 18:23:38 -0400

    DP-72-Add and style the link, title and text

--- src/app/features/committee-account-settings/committee-account-settings.module.ts
+++ src/app/features/committee-account-settings/committee-account-settings.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommitteeAccountSettingsComponent } from './committee-account-settings.component';
 import { CommitteeAccountSettingsRoutingModule } from './committee-account-settings-routing.module';
+import { ReturnLinkModule } from 'src/app/shared/return-link/return-link.module';
 
 @NgModule({
   declarations: [CommitteeAccountSettingsComponent],
-  imports: [CommonModule, CommitteeAccountSettingsRoutingModule],
+  imports: [
+    CommonModule,
+    CommitteeAccountSettingsRoutingModule,
+    ReturnLinkModule,
+  ],
 })
 export class CommitteeAccountSettingsModule {}
