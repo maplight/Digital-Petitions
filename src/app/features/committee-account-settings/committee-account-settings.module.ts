73013bea867153f2842850ee5726ceb2652ccc80
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-13 19:07:27 -0400

    DP-72-Add calls to dialogs

--- src/app/features/committee-account-settings/committee-account-settings.module.ts
+++ src/app/features/committee-account-settings/committee-account-settings.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { CommitteeAccountSettingsComponent } from './committee-account-settings.component';
 import { CommitteeAccountSettingsRoutingModule } from './committee-account-settings-routing.module';
 import { ReturnLinkModule } from 'src/app/shared/return-link/return-link.module';
+import { BasicCardModule } from 'src/app/shared/basic-card/basic-card.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [CommitteeAccountSettingsComponent],
@@ -10,6 +12,8 @@ import { ReturnLinkModule } from 'src/app/shared/return-link/return-link.module'
     CommonModule,
     CommitteeAccountSettingsRoutingModule,
     ReturnLinkModule,
+    BasicCardModule,
+    MatButtonModule,
   ],
 })
 export class CommitteeAccountSettingsModule {}
