af01de23e8b42c56efb6fddfc98189a27639e252
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-14 11:49:28 -0400

    DP-65-Create a module and a component for the avatar

---
+++ src/app/core/layout/avatar/avatar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AvatarComponent } from './avatar.component';
+
+
+
+@NgModule({
+  declarations: [
+    AvatarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AvatarModule { }
