102e0fbb3fd68db6931b196a6cbef21a61ad590d
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-14 06:57:56 -0400

    DP-65-Add the module and the component for the user menu

---
+++ src/app/core/layout/user-menu/user-menu.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserMenuComponent } from './user-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserMenuModule { }
