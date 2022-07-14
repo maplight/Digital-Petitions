9f2a6f578758b4a6824f07e938e628f6ed2b0a4c
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-14 14:28:42 -0400

    DP-65-Fix default values of some variables

--- src/app/core/layout/avatar/avatar.component.ts
+++ src/app/core/layout/avatar/avatar.component.ts
@@ -10,9 +10,6 @@ export class AvatarComponent implements OnInit {
   public first_name: string = 'Map';
   @Input()
   public last_name: string = 'Ligth';
-  /*protected url: string =
-    'https://scontent-mia3-2.xx.fbcdn.net/v/t1.6435-9/159920232_2805029483068326_8468044973819636209_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=0gyKWY-at9oAX9-iCoY&_nc_ht=scontent-mia3-2.xx&oh=00_AT8mHJfHp3NUBIWyfD7KqZoXy1V4IeIv1Ip-SeLu9ZV08w&oe=62F51DC0';
-  */
   @Input()
   public url: string = '';
   protected l_avatar: string =
--- src/app/core/layout/user-menu/user-menu.component.ts
+++ src/app/core/layout/user-menu/user-menu.component.ts
@@ -7,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserMenuComponent implements OnInit {
   protected is_logged: boolean = true;
-  protected first_name: string = 'Denismay';
-  protected last_name: string = 'Concepcion';
-  protected img_url: string =
-    'https://scontent-mia3-2.xx.fbcdn.net/v/t1.6435-9/159920232_2805029483068326_8468044973819636209_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=0gyKWY-at9oAX9-iCoY&_nc_ht=scontent-mia3-2.xx&oh=00_AT8mHJfHp3NUBIWyfD7KqZoXy1V4IeIv1Ip-SeLu9ZV08w&oe=62F51DC0';
+  protected first_name: string = 'James';
+  protected last_name: string = 'Philby';
+  protected img_url: string = '';
   protected email: string = '';
   constructor() {}
 
