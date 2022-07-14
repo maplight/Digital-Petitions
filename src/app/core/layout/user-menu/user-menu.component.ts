29f2c47b2d4b037ae938dd4aa07759c9aa69f81c
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-14 08:08:07 -0400

    DP-65-Add component logic

--- src/app/core/layout/user-menu/user-menu.component.ts
+++ src/app/core/layout/user-menu/user-menu.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'dp-user-menu',
   templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss']
+  styleUrls: ['./user-menu.component.scss'],
 })
 export class UserMenuComponent implements OnInit {
+  protected is_logged: boolean = true;
+  protected name: string = '';
+  protected email: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/core/layout/user-menu/user-menu.module.ts
+++ src/app/core/layout/user-menu/user-menu.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserMenuComponent } from './user-menu.component';
-
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    UserMenuComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UserMenuComponent],
+  imports: [CommonModule, MatMenuModule, MatButtonModule, RouterModule],
+  exports: [UserMenuComponent],
 })
-export class UserMenuModule { }
+export class UserMenuModule {}
