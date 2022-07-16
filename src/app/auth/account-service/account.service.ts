0e8b06f567473162c56d565aa432fd7c331aedd4
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-16 09:06:59 -0400

    DP-52-Create authentication service

---
+++ src/app/auth/account-service/account.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { API, Auth } from 'aws-amplify';
+import { delay, Observable, of } from 'rxjs';
+import { SignUpForm } from '../sign-up/sign-up-form.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  constructor() {}
+  public signIn() {}
+
+  public signUp(data: SignUpForm): Observable<any> {
+    //return of({ result: ':)' }).pipe(delay(3000));
+    return of({ error: ":'(" }).pipe(delay(3000));
+  }
+
+  public signOut() {}
+  public changeEmail() {}
+  public changePassword() {}
+  public changePersonalDetails() {}
+}
