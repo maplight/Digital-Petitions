0c74b557b3435d4f74b5b90a02a555cc4501d103
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-19 14:37:05 -0400

    DP-56-Add API call functionality for sign-in

--- src/app/auth/account-service/account.service.ts
+++ src/app/auth/account-service/account.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { API, Auth } from 'aws-amplify';
 import { delay, Observable, of } from 'rxjs';
 import { Result } from 'src/app/core/application/Result';
+import { SignInForm } from '../sign-in/sign-in-form.interface';
 import { SignUpForm } from '../sign-up/sign-up-form.interface';
 
 @Injectable({
@@ -9,9 +10,12 @@ import { SignUpForm } from '../sign-up/sign-up-form.interface';
 })
 export class AccountService {
   constructor() {}
-  public signIn() {}
 
   public signUp(data: SignUpForm): Observable<Result<string>> {
+    return of({ error: ":'(" }).pipe(delay(3000));
+  }
+
+  public signIn(data: SignInForm): Observable<Result<string>> {
     return of({ error: ":'(" }).pipe(delay(3000));
   }
 
