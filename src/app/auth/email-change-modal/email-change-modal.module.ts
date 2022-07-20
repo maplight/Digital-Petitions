6100f3020fab72a4899fab2d117f9296084c9ba4
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-20 08:36:45 -0400

    DP-74-Bind the change-email modal to the service

--- src/app/auth/email-change-modal/email-change-modal.module.ts
+++ src/app/auth/email-change-modal/email-change-modal.module.ts
@@ -7,6 +7,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ConfirmEmailChangeModalModule } from '../confirm-email-change-modal/confirm-email-change-modal.module';
+import { DialogResultModule } from 'src/app/shared/dialog-result/dialog-result.module';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [EmailChangeModalComponent],
@@ -15,10 +17,12 @@ import { ConfirmEmailChangeModalModule } from '../confirm-email-change-modal/con
     FormsModule,
     ReactiveFormsModule,
     ConfirmEmailChangeModalModule,
+    DialogResultModule,
     BasicModalModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatProgressBarModule,
   ],
 })
 export class EmailChangeModalModule {}
