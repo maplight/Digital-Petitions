b4e28be73c0bcb95c21a44dce828146d5bfeff76
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-20 08:36:15 -0400

    DP-74-Add API call functionality for change-email

--- src/app/auth/account-service/account.service.ts
+++ src/app/auth/account-service/account.service.ts
@@ -4,6 +4,7 @@ import { delay, Observable, of } from 'rxjs';
 import { Result } from 'src/app/core/application/Result';
 import { ChangePasswordForm } from '../change-password-modal/change-password-form.interface';
 import { PersonalDetailsChangeForm } from '../change-personal-details-modal/personal-details-change-form.interface';
+import { EmailChangeForm } from '../email-change-modal/email-change-form.interface';
 import { SignInForm } from '../sign-in/sign-in-form.interface';
 import { SignUpForm } from '../sign-up/sign-up-form.interface';
 
@@ -31,6 +32,9 @@ export class AccountService {
     return of({ result: ":')" }).pipe(delay(3000));
   }
 
+  public changeEmail(data: EmailChangeForm): Observable<Result<string>> {
+    return of({ result: ':)' }).pipe(delay(3000));
+  }
+
   public signOut() {}
-  public changeEmail() {}
 }
