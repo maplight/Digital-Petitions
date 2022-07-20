6e277035938d16b0641f71b2e8fb816abcdc235b
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-20 09:15:37 -0400

    DP-74-Add API call functionality for confirm-change-email

--- src/app/auth/account-service/account.service.ts
+++ src/app/auth/account-service/account.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
-import { API, Auth } from 'aws-amplify';
+
 import { delay, Observable, of } from 'rxjs';
 import { Result } from 'src/app/core/application/Result';
+
 import { ChangePasswordForm } from '../change-password-modal/change-password-form.interface';
 import { PersonalDetailsChangeForm } from '../change-personal-details-modal/personal-details-change-form.interface';
 import { EmailChangeForm } from '../email-change-modal/email-change-form.interface';
 import { SignInForm } from '../sign-in/sign-in-form.interface';
+
+import { ConfirmEmailChangeForm } from '../confirm-email-change-modal/confirm-email-change-form.interface';
+
 import { SignUpForm } from '../sign-up/sign-up-form.interface';
 
 @Injectable({
@@ -33,6 +37,12 @@ export class AccountService {
   }
 
   public changeEmail(data: EmailChangeForm): Observable<Result<string>> {
+    return of({ result: ':)' }).pipe(delay(3000));
+  }
+
+  public confirmEmailChange(
+    data: ConfirmEmailChangeForm
+  ): Observable<Result<string>> {
     return of({ result: ':)' }).pipe(delay(3000));
   }
 
