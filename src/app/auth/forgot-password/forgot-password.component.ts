0640cdc3af281b0619a685f60ab83259f4cbe12b
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-21 16:47:42 -0400

    DP-68-Create the module and component for forgot-password

---
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-forgot-password',
+  templateUrl: './forgot-password.component.html',
+})
+export class ForgotPasswordComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/auth/forgot-password/forgot-password.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ForgotPasswordComponent } from './forgot-password.component';
+import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
+
+@NgModule({
+  declarations: [ForgotPasswordComponent],
+  imports: [CommonModule, ForgotPasswordRoutingModule],
+})
+export class ForgotPasswordModule {}
