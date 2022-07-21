f2c53e7968e93f4fdba1f125e2116fc628ba4015
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-21 17:31:43 -0400

    DP-68-Add the layout elements to the forgot-password component

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,11 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Subject, tap } from 'rxjs';
+import { SignInService } from 'src/app/core/application/sign-in.service';
+import { AccountService } from '../account-service/account.service';
+import { ForgotPasswordForm } from './forgot-password-form.interface';
 
 @Component({
   selector: 'dp-forgot-password',
   templateUrl: './forgot-password.component.html',
 })
-export class ForgotPasswordComponent implements OnInit {
-  constructor() {}
+export class ForgotPasswordComponent implements OnInit, OnDestroy {
+  protected hide_password = true;
+
+  public formGroup: FormGroup;
+  public form_data: ForgotPasswordForm = {
+    email: new FormControl('', [Validators.required, Validators.email]),
+  };
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group(this.form_data);
+  }
 
   ngOnInit(): void {}
+  ngOnDestroy(): void {}
+
+  submit() {
+    if (this.formGroup.valid) {
+      //call service
+    }
+  }
 }
--- src/app/auth/forgot-password/forgot-password.module.ts
+++ src/app/auth/forgot-password/forgot-password.module.ts
@@ -2,9 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForgotPasswordComponent } from './forgot-password.component';
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
+import { BasicCardModule } from 'src/app/shared/basic-card/basic-card.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReturnLinkModule } from 'src/app/shared/return-link/return-link.module';
 
 @NgModule({
   declarations: [ForgotPasswordComponent],
-  imports: [CommonModule, ForgotPasswordRoutingModule],
+  imports: [
+    CommonModule,
+    ForgotPasswordRoutingModule,
+    BasicCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ReturnLinkModule,
+  ],
 })
 export class ForgotPasswordModule {}
