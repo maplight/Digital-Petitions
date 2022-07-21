8d81ac94085bbc58e83bb1beea32245ab93266f1
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-21 07:13:33 -0400

    DP-66-Create service to manage sign-out functionality

---
+++ src/app/core/application/sign-out.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import {
+  exhaustMap,
+  map,
+  merge,
+  Observable,
+  partition,
+  shareReplay,
+  Subject,
+  tap,
+} from 'rxjs';
+import { AccountService } from 'src/app/auth/account-service/account.service';
+import { Result } from './Result';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SignOutService {
+  public error$: Observable<Result<string>>;
+  public success$: Observable<Result<string>>;
+  public loading$: Observable<boolean>;
+  public result$: Observable<Result<string>>;
+  private submit$: Subject<void> = new Subject();
+
+  constructor(private AccountService: AccountService) {
+    this.result$ = this.submit$.pipe(
+      exhaustMap((data) => this.AccountService.signOut()),
+      shareReplay(1)
+    );
+    const [success$, error$] = partition(this.result$, (value) =>
+      value.result ? true : false
+    );
+
+    this.success$ = success$.pipe(
+      //redirect
+      //map((value) => value.result),
+      tap((value) => console.log(value)),
+      shareReplay(1)
+    );
+
+    this.error$ = error$.pipe(
+      //map((value) => value.error),
+      tap((value) => console.log(value)),
+      shareReplay(1)
+    );
+
+    const end$ = merge(this.success$, this.error$);
+
+    this.loading$ = merge(
+      this.submit$.pipe(
+        map((v) => true),
+        tap(() => console.log('start'))
+      ),
+      end$.pipe(
+        map((v) => false),
+        tap(() => console.log('end'))
+      )
+    ).pipe(shareReplay(1));
+  }
+  ngOnDestroy(): void {
+    this.submit$.complete();
+  }
+
+  signOut() {
+    this.submit$.next();
+  }
+}
