f0fea6916b0d05e640e346b1d5d9a87e40bdbbea
lunox94 <gmrlunox94@gmail.com>
2022-07-22 17:16:47 -0400

    chore - remove unused imports

--- src/app/auth/account-service/account.service.ts
+++ src/app/auth/account-service/account.service.ts
@@ -3,17 +3,16 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, delay, Observable, of } from 'rxjs';
 import { Result } from 'src/app/core/application/Result';
 
-import { ChangePasswordForm } from '../change-password-modal/change-password-form.interface';
-import { PersonalDetailsChangeForm } from '../change-personal-details-modal/personal-details-change-form.interface';
-import { EmailChangeForm } from '../email-change-modal/email-change-form.interface';
-import { SignInForm } from '../sign-in/sign-in-form.interface';
-
-import { ConfirmEmailChangeForm } from '../confirm-email-change-modal/confirm-email-change-form.interface';
-
 import { User } from '../user';
-import { SetNewPasswordForm } from '../set-new-password/set-new-password-form.interface';
-import { ForgotPasswordForm } from '../forgot-password/forgot-password-form.interface';
-import { ChangeEmailData, ChangePasswordData, ConfirmationCode, PersonalDetailsToUpdate, RecoverPasswordData, SignInCredentials, SignUpCredentials } from 'src/app/core/models/models';
+import {
+  ChangeEmailData,
+  ChangePasswordData,
+  ConfirmationCode,
+  PersonalDetailsToUpdate,
+  RecoverPasswordData,
+  SignInCredentials,
+  SignUpCredentials,
+} from 'src/app/core/models/models';
 
 @Injectable({
   providedIn: 'root',
