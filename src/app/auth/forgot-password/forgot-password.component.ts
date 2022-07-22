1ad02cec95cfff249146d3b48e76e10456fd76d9
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-22 08:23:24 -0400

    DP-68-bind the forgot-password component to its service

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -6,6 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { Subject, tap } from 'rxjs';
+import { ForgotPasswordService } from 'src/app/core/application/forgot-password.service';
 import { SignInService } from 'src/app/core/application/sign-in.service';
 import { AccountService } from '../account-service/account.service';
 import { ForgotPasswordForm } from './forgot-password-form.interface';
@@ -17,20 +18,40 @@ import { ForgotPasswordForm } from './forgot-password-form.interface';
 export class ForgotPasswordComponent implements OnInit, OnDestroy {
   protected hide_password = true;
 
+  protected result$;
+  protected loading$;
+  private _unsubscribeAll: Subject<void> = new Subject();
+
   public formGroup: FormGroup;
   public form_data: ForgotPasswordForm = {
     email: new FormControl('', [Validators.required, Validators.email]),
   };
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private ForgotPasswordService: ForgotPasswordService,
+    private AccountService: AccountService
+  ) {
     this.formGroup = this.formBuilder.group(this.form_data);
+    this.result$ = this.ForgotPasswordService.result$.pipe(
+      tap((result) => {
+        if (!!result.result) {
+          /*redirect*/
+          AccountService.updateUser(true);
+        }
+      })
+    );
+    this.loading$ = this.ForgotPasswordService.loading$;
   }
 
   ngOnInit(): void {}
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
 
   submit() {
     if (this.formGroup.valid) {
-      //call service
+      this.ForgotPasswordService.formGroupValue = this.formGroup.value;
     }
   }
 }
