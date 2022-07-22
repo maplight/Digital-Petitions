d2bb967a789ea9583f239e4feea75c44bd70e06d
lunox94 <gmrlunox94@gmail.com>
2022-07-22 00:40:50 -0400

    DP-47 - move aws config to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  awsExports: {
+    aws_appsync_graphqlEndpoint: '',
+    aws_appsync_region: '',
+    aws_appsync_authenticationType: '',
+    aws_appsync_apiKey: '',
+    Auth: {
+      region: '',
+      userPoolId: '',
+      userPoolWebClientId: '',
+    },
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { Amplify } from 'aws-amplify';
 import { AppModule } from './app/app.module';
-import awsmobile from './environments/aws-exports';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
-Amplify.configure(awsmobile);
+
+Amplify.configure(environment.awsExports);
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
