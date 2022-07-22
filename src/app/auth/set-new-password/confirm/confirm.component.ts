2d4244850add88e1735548ee9f98ac492ab510cf
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-21 21:46:50 -0400

    DP-68-Create confirmation component for set-new-password component

---
+++ src/app/auth/set-new-password/confirm/confirm.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-confirm',
+  templateUrl: './confirm.component.html',
+})
+export class ConfirmComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
