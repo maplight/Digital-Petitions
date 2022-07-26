857aa5398c39c8f3aa336232da33dad530a8f167
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-26 10:32:20 -0400

    DP-91-Add new-petition form

--- src/app/features/new-petition/new-petition-routing.module.ts
+++ src/app/features/new-petition/new-petition-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewPetitionIssueComponent } from './new-petition-issue/new-petition-issue.component';
 import { NewPetitionComponent } from './new-petition.component';
 
 const routes: Routes = [
   {
     path: '',
     component: NewPetitionComponent,
+  },
+  {
+    path: 'issue',
+    component: NewPetitionIssueComponent,
   },
 ];
 
