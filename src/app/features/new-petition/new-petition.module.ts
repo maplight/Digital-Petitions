73f81bfbe2e484795fe5f30f9820d2135ad98097
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-26 10:32:51 -0400

    DP-91-Add route for new-petition-issue

--- src/app/features/new-petition/new-petition.module.ts
+++ src/app/features/new-petition/new-petition.module.ts
@@ -2,9 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewPetitionComponent } from './new-petition.component';
 import { NewPetitionRoutingModule } from './new-petition-routing.module';
+import { BasicCardModule } from 'src/app/shared/basic-card/basic-card.module';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { NewPetitionIssueModule } from './new-petition-issue/new-petition-issue.module';
+import { RouterModule } from '@angular/router';
+import { StepIndicatorModule } from './step-indicator/step-indicator.module';
 
 @NgModule({
   declarations: [NewPetitionComponent],
-  imports: [CommonModule, NewPetitionRoutingModule],
+  imports: [
+    CommonModule,
+    NewPetitionRoutingModule,
+    BasicCardModule,
+    MatRadioModule,
+    FormsModule,
+    MatButtonModule,
+    NewPetitionIssueModule,
+    RouterModule,
+    StepIndicatorModule,
+  ],
 })
 export class NewPetitionModule {}
