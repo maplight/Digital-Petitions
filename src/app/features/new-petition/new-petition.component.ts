be840d84fc196f1b09be4d3301a940091354a305
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-26 21:19:11 -0400

    DP-94-Bind the new-petition component to its functions

--- src/app/features/new-petition/new-petition.component.ts
+++ src/app/features/new-petition/new-petition.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IssuePetitionData } from 'src/app/shared/models/exports';
 
 @Component({
   selector: 'dp-new-petition',
   templateUrl: './new-petition.component.html',
 })
 export class NewPetitionComponent implements OnInit {
-  typePetition: string = '';
-  types: string[] = ['Candidate', 'Issue'];
-  constructor(private _router: Router, private _route: ActivatedRoute) {}
+  protected currentStep: '1' | '21' | '22' | '3' = '1';
+  protected dataResponseIssue: IssuePetitionData = { title: '', text: '' };
+  constructor() {}
 
   ngOnInit(): void {}
 
-  submit() {
-    if (this.typePetition === 'Issue') {
-      this._router.navigate(['issue'], { relativeTo: this._route });
+  cancel() {}
+
+  submit1(data: string) {
+    if (data === 'Issue') {
+      this.currentStep = '21';
+    } else if (data === 'Candidate') {
+      this.currentStep = '22';
     }
+  }
+
+  submit21(data: IssuePetitionData) {
+    this.dataResponseIssue = data;
+    this.currentStep = '3';
   }
 }
