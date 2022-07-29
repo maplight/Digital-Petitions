8f99ea5ac3dd65926b25c4e2bea38b55817042c4
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-29 18:59:08 -0400

    DP-118-Add backen call methods to edit requests

--- src/app/features/new-petition/petition-service/petition.service.ts
+++ src/app/features/new-petition/petition-service/petition.service.ts
@@ -5,6 +5,7 @@ import {
   IssuePetitionData,
   Result,
 } from 'src/app/shared/models/exports';
+import { ResponsePetition } from 'src/app/shared/models/petition/response-petition';
 
 @Injectable({
   providedIn: 'root',
@@ -22,5 +23,21 @@ export class PetitionService {
     data: CandidatePetitionData
   ): Observable<Result<CandidatePetitionData>> {
     return of({ result: data }).pipe(delay(3000));
+  }
+  editPetitionIssue(
+    data: IssuePetitionData
+  ): Observable<Result<IssuePetitionData>> {
+    return of({ result: data }).pipe(delay(3000));
+  }
+
+  editPetitionCandidate(
+    data: CandidatePetitionData
+  ): Observable<Result<CandidatePetitionData>> {
+    return of({ result: data }).pipe(delay(3000));
+  }
+  getPetition(data: number): Observable<Result<ResponsePetition>> {
+    return of({ result: { dataIssue: { title: 'Title', text: 'Text' } } }).pipe(
+      delay(3000)
+    );
   }
 }
