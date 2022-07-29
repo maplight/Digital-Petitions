bfc20b8481e0d5f298d5010f1c2898c565bf4b0a
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-28 21:01:06 -0400

    DP-49-Refactor the avatar

--- src/app/core/layout/avatar/avatar.component.ts
+++ src/app/core/layout/avatar/avatar.component.ts
@@ -5,7 +5,6 @@ import { AccountService } from 'src/app/auth/account-service/account.service';
 @Component({
   selector: 'dp-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.scss'],
 })
 export class AvatarComponent implements OnInit {
   private _unsubscribeAll: Subject<void> = new Subject();
@@ -17,11 +16,9 @@ export class AvatarComponent implements OnInit {
       .pipe(
         tap((data) => {
           if (!!data) {
-            const name: { firstName: string; lastName: string } = JSON.parse(
-              data.attributes.name
-            );
             this.l_avatar =
-              name.firstName[0].toUpperCase() + name.lastName[0].toUpperCase();
+              data.attributes.given_name[0].toUpperCase() +
+              data.attributes.family_name[0].toUpperCase();
           }
         }),
         takeUntil(this._unsubscribeAll)
