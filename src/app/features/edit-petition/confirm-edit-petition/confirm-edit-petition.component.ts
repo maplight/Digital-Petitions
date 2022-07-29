c5e87e053b1c9a3e64f5d4e54f4ac320f1211b6b
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-07-29 07:42:27 -0400

    DP-116-Create the confirm-edit component

---
+++ src/app/features/edit-petition/confirm-edit-petition/confirm-edit-petition.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-confirm-edit-petition',
+  templateUrl: './confirm-edit-petition.component.html',
+})
+export class ConfirmEditPetitionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submit() {}
+}
