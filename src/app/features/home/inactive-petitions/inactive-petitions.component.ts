5194162101f281e2bbdbc4078f0cef39c1f3c9b4
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-03 09:06:32 -0400

    DP-127-Add the inactive-petitions component

---
+++ src/app/features/home/inactive-petitions/inactive-petitions.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-inactive-petitions',
+  templateUrl: './inactive-petitions.component.html',
+})
+export class InactivePetitionsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/features/home/inactive-petitions/inactive-petitions.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InactivePetitionsComponent } from './inactive-petitions.component';
+
+
+
+@NgModule({
+  declarations: [
+    InactivePetitionsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InactivePetitionsModule { }
