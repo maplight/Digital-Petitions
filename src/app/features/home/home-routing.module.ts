07ad6f069ef0c35e106b2e0f8e717d76d2c8cb69
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-03 09:07:57 -0400

    DP-127-Add route for inactive petitions

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from 'src/app/city-staff/home/home.component';
+import { HomeComponent } from './home.component';
+import { InactivePetitionsComponent } from './inactive-petitions/inactive-petitions.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'inactive',
+    component: InactivePetitionsComponent,
   },
 ];
 
