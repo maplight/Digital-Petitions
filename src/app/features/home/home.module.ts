0aa9530390f9c156080289db74f144112f00bfdf
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-03 09:04:42 -0400

    DP-127-Add the interface elements of home-component

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { PetitionCardModule } from 'src/app/shared/petition-card/petition-card.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, PetitionCardModule],
 })
 export class HomeModule {}
