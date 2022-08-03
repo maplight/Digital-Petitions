87d18ea871d35fc0c7cef79da6c7d7fac0cd674d
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-03 10:26:08 -0400

    DP-127-Fix some links

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { PetitionCardModule } from 'src/app/shared/petition-card/petition-card.module';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, PetitionCardModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    PetitionCardModule,
+    MatButtonModule,
+    RouterModule,
+  ],
 })
 export class HomeModule {}
