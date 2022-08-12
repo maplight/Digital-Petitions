ec07193925bbab6e4944bce1ee858cf917e6d1b8
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-12 14:57:25 -0400

    DP-157-Fix logic of show/hide continue button in verify-sign

--- src/app/features/sign-petition/verify-sign/verify-sign.component.ts
+++ src/app/features/sign-petition/verify-sign/verify-sign.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map, merge, Observable, tap } from 'rxjs';
 
 import { SignaturePetitionType } from 'src/app/shared/models/petition/signature-petition-type';
 
@@ -10,6 +11,7 @@ import { SignaturePetitionType } from 'src/app/shared/models/petition/signature-
 export class VerifySignComponent implements OnInit {
   protected formGroup: FormGroup;
   protected formGroupLicense: FormGroup;
+  protected isContinue$: Observable<boolean>;
   @Output() submitEvent: EventEmitter<SignaturePetitionType> =
     new EventEmitter<SignaturePetitionType>();
   @Output() cancelEvent: EventEmitter<
@@ -25,12 +27,27 @@ export class VerifySignComponent implements OnInit {
       licenseNumber: ['', [Validators.required]],
       dateOfBirth: ['', [Validators.required]],
     });
+    this.isContinue$ = merge(
+      this.formGroup.valueChanges,
+      this.formGroupLicense.valueChanges
+    ).pipe(
+      map(() => {
+        return this.formGroup.valid
+          ? this.formGroup.value.verifyType === 'license'
+            ? this.formGroupLicense.valid
+              ? true
+              : false
+            : true
+          : false;
+      })
+    );
   }
 
   ngOnInit(): void {}
   cancel(value: 'loading' | 'verify' | 'empty' | 'view' | 'sign' | 'error') {
     this.cancelEvent.emit(value);
   }
+
   submit() {
     if (this.formGroup.valid) {
       if (this.formGroup.value.verifyType === 'license') {
