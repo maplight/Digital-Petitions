7445afa5e364ffb97a79747e7e455844e33badb9
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-18 22:08:00 -0400

    DP-177-Create a component to display AwaitingApproval petition actions

---
+++ src/app/features/view-petition-city-staff/new-box/new-box.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-new-box',
+  templateUrl: './new-box.component.html',
+})
+export class NewBoxComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
