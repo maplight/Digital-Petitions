c79c11ed6aeb9d39f7f88f05b8aee178897c30b0
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-19 17:07:28 -0400

    DP-174-Add the interface elements to deny a petition

--- src/app/features/view-petition-city-staff/deny-alert/deny-alert.module.ts
+++ src/app/features/view-petition-city-staff/deny-alert/deny-alert.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DenyAlertComponent } from './deny-alert.component';
-
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { BasicAlertModule } from 'src/app/shared/basic-alert/basic-alert.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    DenyAlertComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DenyAlertComponent],
+  imports: [CommonModule, MatDialogModule, BasicAlertModule, MatButtonModule],
+  exports: [DenyAlertComponent],
 })
-export class DenyAlertModule { }
+export class DenyAlertModule {}
