b9dc798a9a91571bdd84d370183bb41b3ce440f4
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-19 16:45:41 -0400

    DP-173-Create the deny-alert component

---
+++ src/app/features/view-petition-city-staff/deny-alert/deny-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dp-deny-alert',
+  templateUrl: './deny-alert.component.html',
+})
+export class DenyAlertComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/features/view-petition-city-staff/deny-alert/deny-alert.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DenyAlertComponent } from './deny-alert.component';
+
+
+
+@NgModule({
+  declarations: [
+    DenyAlertComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DenyAlertModule { }
