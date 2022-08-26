7eb7d6702b64fb52109497db6409f02ed11df45e
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-26 15:24:49 -0400

    DP-138-Update GraphQL configuration files

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -2,6 +2,41 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getPetition = /* GraphQL */ `
+  query GetPetition($PK: ID!) {
+    getPetition(PK: $PK) {
+      PK
+      createdAt
+      owner
+      signatureSummary {
+        approved
+        deadline
+        required
+        submitted
+      }
+      status
+      type
+      updatedAt
+      version
+      ... on CandidatePetition {
+        address {
+          address
+          city
+          number
+          state
+          zipCode
+        }
+        name
+        office
+        party
+      }
+      ... on IssuePetition {
+        detail
+        title
+      }
+    }
+  }
+`;
 export const getPetitionsByOwner = /* GraphQL */ `
   query GetPetitionsByOwner($query: PetitionsByOwnerInput) {
     getPetitionsByOwner(query: $query) {
