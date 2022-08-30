11c5b56d35e4e0c1154b4f6fdfc356d3e5cb4db8
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-29 20:59:18 -0400

    DP-198-Add interface elements to city-staff-admin

--- src/app/features/city-staff-admin/city-staff-admin.module.ts
+++ src/app/features/city-staff-admin/city-staff-admin.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CityStaffAdminComponent } from './city-staff-admin.component';
 import { CityStaffAdminRoutingModule } from './city-staff-admin-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CityStaffAdminComponent],
-  imports: [CommonModule, CityStaffAdminRoutingModule],
+  imports: [
+    CommonModule,
+    CityStaffAdminRoutingModule,
+    MatButtonModule,
+    RouterModule,
+  ],
 })
 export class CityStaffAdminModule {}
