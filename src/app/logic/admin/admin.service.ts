d9a78e2127266d66b9f8b9ea76362a4df36ebfca
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-08-31 09:25:07 -0400

    DP-201-Add an api call function to add a new member

--- src/app/logic/admin/admin.service.ts
+++ src/app/logic/admin/admin.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
 import { Member } from 'src/app/shared/models/admin/member';
+import { NewMemberData } from 'src/app/shared/models/admin/new-member-data';
 import { Result } from 'src/app/shared/models/exports';
 
 @Injectable({
@@ -13,16 +14,36 @@ export class AdminService {
     return of({
       result: [
         {
+          id: '0',
           email: 'marvin.mckinney@email.com',
           name: 'Marvin McKinney (You)',
           status: 'Admin',
         },
         {
+          id: '0',
           email: 'marvin.mckinney2@email.com',
           name: 'Marvin McKinney (Not You)',
           status: 'Member',
         },
       ],
+    }).pipe(delay(3000));
+  }
+  newMember(data: NewMemberData): Observable<Result<string>> {
+    return of({
+      result: 'SUCCESS',
+    }).pipe(delay(3000));
+  }
+  changeAccountPermission(data: {
+    id: string;
+    value: 'admin' | 'guest' | 'member';
+  }): Observable<Result<string>> {
+    return of({
+      result: 'SUCCESS',
+    }).pipe(delay(3000));
+  }
+  getAccountPermission(id: string): Observable<Result<string>> {
+    return of({
+      result: 'member',
     }).pipe(delay(3000));
   }
 }
