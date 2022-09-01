82c54dcd03f970e47cfd0745e3dba58124ee1d97
lunox94 <gmrlunox94@gmail.com>
2022-08-31 23:27:02 -0400

    chore - polish sign in feature

--- src/app/logic/auth/sign-in.service.ts
+++ src/app/logic/auth/sign-in.service.ts
@@ -12,20 +12,20 @@ import {
 } from 'rxjs';
 import { AccountService } from 'src/app/core/account-service/account.service';
 import { LoggingService } from 'src/app/core/logging/loggin.service';
-import { CognitoUserLite } from 'src/app/shared/models/auth/user';
+import { CognitoUserFacade } from 'src/app/shared/models/auth/user';
 import { Result, SignInCredentials } from 'src/app/shared/models/exports';
 
 @Injectable()
 export class SignInService implements OnDestroy {
   public error$: Observable<string | undefined>;
   public loading$: Observable<boolean>;
-  public result$: Observable<Result<CognitoUserLite>>;
+  public result$: Observable<Result<CognitoUserFacade>>;
   private _submit: Subject<SignInCredentials> = new Subject();
-  public success$: Observable<CognitoUserLite | undefined>;
+  public success$: Observable<CognitoUserFacade | undefined>;
 
   constructor(
     private _accountService: AccountService,
-    private _loggingService: LoggingService,
+    private _logger: LoggingService,
     private _router: Router
   ) {
     this.result$ = this._submit.pipe(
@@ -41,8 +41,7 @@ export class SignInService implements OnDestroy {
     this.success$ = success$.pipe(
       map((value) => value.result),
       tap((value) => {
-        this._loggingService.log(value);
-        switch (value?.attributes.custom.access_group) {
+        switch (value?.attributes?.['custom:access_group']) {
           case 'petitioner':
             this._router.navigate(['/committee/home']);
             break;
@@ -54,7 +53,7 @@ export class SignInService implements OnDestroy {
 
     this.error$ = error$.pipe(
       map((value) => value.error),
-      tap((value) => this._loggingService.log(value)),
+      tap((value) => this._logger.log(value)),
       shareReplay(1)
     );
 
@@ -62,12 +61,12 @@ export class SignInService implements OnDestroy {
 
     this.loading$ = merge(
       this._submit.pipe(
-        map((v) => true),
-        tap(() => console.log('start'))
+        map((_) => true),
+        tap((_) => this._logger.log('start'))
       ),
       end$.pipe(
-        map((v) => false),
-        tap(() => console.log('end'))
+        map((_) => false),
+        tap((_) => this._logger.log('end'))
       )
     ).pipe(shareReplay(1));
   }
@@ -76,9 +75,10 @@ export class SignInService implements OnDestroy {
     this._submit.complete();
   }
 
-  /** This method begins a user's authentication process.
-  @param value: SignInCredentials type object, contains email and password data provided by the user.
-  */
+  /**
+   * This method begins a user's authentication process.
+   * @param value SignInCredentials type object, contains email and password data provided by the user.
+   */
   requestSignIn(value: SignInCredentials) {
     this._submit.next(value);
   }
