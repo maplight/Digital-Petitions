51f8fad86b30d741d0bf12caabdc4632d5e5088e
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-09-06 21:17:12 -0400

    DP-216-Create a custom color picker

---
+++ src/app/features/city-staff-site-design/color-picker/color-picker.component.ts
@@ -0,0 +1,26 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { __importDefault } from 'tslib';
+
+@Component({
+  selector: 'dp-color-picker',
+  templateUrl: './color-picker.component.html',
+})
+export class ColorPickerComponent {
+  protected hue!: string | null;
+  @Input() color!: string | null;
+  @Output() eventColor: EventEmitter<string | null> = new EventEmitter();
+  sendColor(value: string | null) {
+    this.color = value;
+    this.eventColor.emit(value);
+  }
+}
