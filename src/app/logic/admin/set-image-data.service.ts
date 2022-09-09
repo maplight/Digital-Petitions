a5dda192edda034d93fc3bbe881802cc82297ac9
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-09-09 09:55:07 -0400

    DP-220-Create service to send a logo to the backend

---
+++ src/app/logic/admin/set-image-data.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import {
+  exhaustMap,
+  map,
+  merge,
+  Observable,
+  partition,
+  shareReplay,
+  Subject,
+  tap,
+} from 'rxjs';
+import { StaffUserInput } from 'src/app/core/api/API';
+import { LoggingService } from 'src/app/core/logging/loggin.service';
+import { Result } from 'src/app/shared/models/exports';
+import { AdminService } from './admin.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SetImageDataService {
+  public error$: Observable<string | undefined>;
+  public success$: Observable<string | undefined>;
+  public loading$: Observable<boolean>;
+  public result$: Observable<Result<string>>;
+  private submit$: Subject<{ url: string; img: ArrayBuffer }> = new Subject();
+
+  constructor(
+    private _adminLogic: AdminService,
+    private _loggingService: LoggingService
+  ) {
+    this.result$ = this.submit$.pipe(
+      exhaustMap((data) => this._adminLogic.setImg(data)),
+      shareReplay(1)
+    );
+    const [success$, error$] = partition(this.result$, (value) =>
+      value.result ? true : false
+    );
+
+    this.success$ = success$.pipe(
+      map((value) => value.result),
+      tap((value) => this._loggingService.log(value)),
+      shareReplay(1)
+    );
+
+    this.error$ = error$.pipe(
+      map((value) => value.error),
+      tap((value) => this._loggingService.log(value)),
+      shareReplay(1)
+    );
+
+    const end$ = merge(this.success$, this.error$);
+
+    this.loading$ = merge(
+      this.submit$.pipe(
+        map((v) => true),
+        tap(() => console.log('start'))
+      ),
+      end$.pipe(
+        map((v) => false),
+        tap(() => console.log('end'))
+      )
+    ).pipe(shareReplay(1));
+  }
+  ngOnDestroy(): void {
+    this.submit$.complete();
+  }
+
+  setImageData(value: { url: string; img: ArrayBuffer }) {
+    this.submit$.next(value);
+  }
+}
