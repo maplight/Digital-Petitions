e78fcf4be6dc3db02aab5ebf2e70ffeb1fd6677b
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-09-10 09:01:24 -0400

    DP-230-Fix logged in user access to system resources

--- src/app/guards/noAuth.guard.ts
+++ src/app/guards/noAuth.guard.ts
@@ -24,8 +24,10 @@ export class NoAuthGuard implements CanActivate {
     | UrlTree {
     return this._auth.isLoged().pipe(
       map((data) => {
-        if (data) {
-          return this._router.parseUrl('/committee/account-settings');
+        if (data?.attributes['custom:access_group'] === 'petitioner') {
+          return this._router.parseUrl('/committee/home');
+        } else if (data?.attributes['custom:access_group'] === 'admin') {
+          return this._router.parseUrl('/city-staff/home');
         } else {
           return true;
         }
