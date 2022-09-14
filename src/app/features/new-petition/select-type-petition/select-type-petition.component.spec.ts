59948ab7ce5fd5d91fea0d0da4f445f1734630a2
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-09-14 09:10:25 -0400

    DP-247-Add the unit tests of select-type-petition-component

--- src/app/features/new-petition/select-type-petition/select-type-petition.component.spec.ts
+++ src/app/features/new-petition/select-type-petition/select-type-petition.component.spec.ts
@@ -1,4 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BasicCardModule } from 'src/app/shared/basic-card/basic-card.module';
 
 import { SelectTypePetitionComponent } from './select-type-petition.component';
 
@@ -8,9 +15,17 @@ describe('SelectTypePetitionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SelectTypePetitionComponent ]
-    })
-    .compileComponents();
+      declarations: [SelectTypePetitionComponent],
+      imports: [
+        CommonModule,
+        BasicCardModule,
+        MatRadioModule,
+        FormsModule,
+        MatButtonModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SelectTypePetitionComponent);
     component = fixture.componentInstance;
@@ -19,5 +34,34 @@ describe('SelectTypePetitionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show two radio buttons', () => {
+    const element =
+      fixture.debugElement.nativeElement.querySelectorAll('mat-radio-button');
+    expect(element.length).toBe(2);
+  });
+
+  it('should cancelEvent emit when cancel botton is clicked', () => {
+    component.cancelEvent.asObservable().subscribe((_) => {
+      expect(true).toBeTrue();
+    });
+    component.cancel();
+  });
+
+  it('"submitEvent" component should  emit correct object ("Issue") when submit botton is clicked', () => {
+    component.submitEvent.asObservable().subscribe((data) => {
+      expect(data).toEqual('Issue');
+    });
+    component.typePetition = 'Issue';
+    component.submit();
+  });
+
+  it('"submitEvent" component should  emit correct object ("Candidate") when submit botton is clicked', () => {
+    component.submitEvent.asObservable().subscribe((data) => {
+      expect(data).toEqual('Candidate');
+    });
+    component.typePetition = 'Candidate';
+    component.submit();
   });
 });
