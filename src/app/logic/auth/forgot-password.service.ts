e575c0225bd30d7c711906db2ec41723e4200571
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-09-30 09:39:49 -0400

    DP-281-Add unit test for forgot-password component

--- src/app/logic/auth/forgot-password.service.ts
+++ src/app/logic/auth/forgot-password.service.ts
@@ -71,7 +71,7 @@ export class ForgotPasswordService {
   /** This method begins the process to recover a forgotten password
   @param value: Recovery email of the user proportionate
   */
-  formGroupValue(value: RecoverPasswordData) {
+  RecoverPasswordData(value: RecoverPasswordData) {
     this.submit$.next(value);
     this.email = value.email;
   }
