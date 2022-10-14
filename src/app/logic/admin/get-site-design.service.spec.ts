af1b20aeb55a3b3e31672167ebcc0b73ec3a8cbf
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-10-13 22:03:09 -0400

    DP-333-Add unit test for get-site-design service

--- src/app/logic/admin/get-site-design.service.spec.ts
+++ src/app/logic/admin/get-site-design.service.spec.ts
@@ -3,24 +3,95 @@ import { LoggingService } from 'src/app/core/logging/loggin.service';
 import { MockedAdminService } from 'src/testing/mocked-admin-service';
 import { MockedLoggingService } from 'src/testing/mocked-logging-service';
 import { AdminService } from './admin.service';
-
+import { of } from 'rxjs';
 import { GetSiteDesignService } from './get-site-design.service';
 
 describe('GetSiteDesignService', () => {
   let service: GetSiteDesignService;
 
+  let valueServiceSpy: jasmine.SpyObj<AdminService>;
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('AdminService', ['getThemeData']);
     TestBed.configureTestingModule({
       providers: [
         GetSiteDesignService,
-        { provide: AdminService, useClass: MockedAdminService },
+        { provide: AdminService, useValue: spy },
         { provide: LoggingService, useClass: MockedLoggingService },
       ],
     });
     service = TestBed.inject(GetSiteDesignService);
+    valueServiceSpy = TestBed.inject(
+      AdminService
+    ) as jasmine.SpyObj<AdminService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('"result$" should emit a Result<SiteConfiguration> value when getThemeData emit a correct value', () => {
+    valueServiceSpy.getThemeData.and.returnValue(
+      of({
+        result: {
+          __typename: 'SiteConfiguration',
+          version: 1,
+        },
+      })
+    );
+
+    service.result$.subscribe((data) => {
+      expect(data).toEqual({
+        result: {
+          __typename: 'SiteConfiguration',
+          version: 1,
+        },
+      });
+    });
+    service.getSiteThemeData();
+  });
+
+  it('"success$" should emit a <SiteConfiguration> value when getThemeData emit a correct value', () => {
+    valueServiceSpy.getThemeData.and.returnValue(
+      of({
+        result: {
+          __typename: 'SiteConfiguration',
+          version: 1,
+        },
+      })
+    );
+
+    service.success$.subscribe((data) => {
+      expect(data).toEqual({
+        __typename: 'SiteConfiguration',
+        version: 1,
+      });
+    });
+    service.getSiteThemeData();
+  });
+
+  it('"error$" should emit a "string" value when getThemeData emit a error value', () => {
+    valueServiceSpy.getThemeData.and.returnValue(
+      of({
+        error: 'some error',
+      })
+    );
+
+    service.error$.subscribe((data) => {
+      expect(data).toEqual('some error');
+    });
+    service.getSiteThemeData();
+  });
+
+  it('"loading$" should emit a "boolean" value when getThemeData emit any value', () => {
+    valueServiceSpy.getThemeData.and.returnValue(
+      of({
+        error: 'some error',
+      })
+    );
+
+    service.loading$.subscribe((data) => {
+      data ? expect(data).toEqual(true) : expect(data).toEqual(false);
+    });
+
+    service.getSiteThemeData();
   });
 });
