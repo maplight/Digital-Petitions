4a329900902a4cb3186db61ef9d927c4981386e8
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-10-15 09:39:58 -0400

    DP-333-Add unit test for get-theme service

--- src/app/logic/admin/get-theme-data.service.spec.ts
+++ src/app/logic/admin/get-theme-data.service.spec.ts
@@ -5,18 +5,32 @@ import { MockedLoggingService } from 'src/testing/mocked-logging-service';
 import { AdminService } from './admin.service';
 
 import { GetThemeDataService } from './get-theme-data.service';
+import { API } from 'aws-amplify';
 
 describe('GetThemeDataService', () => {
   let service: GetThemeDataService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [GetThemeDataService],
+      providers: [
+        GetThemeDataService,
+        {
+          provide: API,
+          useValue: { graphql: () => Promise.resolve('hello') },
+        },
+      ],
     });
     service = TestBed.inject(GetThemeDataService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('changeAccountPermission should return a User object when the promise it succesful resolve', (done) => {
+    API.graphql = jasmine.createSpy().and.returnValue(of('value'));
+    service.updatedThemeData().subscribe((data) => {
+      expect(data).toEqual('value');
+      done();
+    });
   });
 });
--- src/app/logic/admin/get-theme-data.service.ts
+++ src/app/logic/admin/get-theme-data.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
 import { API, graphqlOperation } from 'aws-amplify';
-import {
-  SiteConfiguration,
-  SiteConfigurationQuery,
-  UpdatedSiteConfigurationSubscription,
-} from 'src/app/core/api/API';
-import { Result } from 'src/app/shared/models/exports';
+
 import { updatedSiteConfiguration } from 'src/graphql/subscriptions';
 import { Observable } from 'zen-observable-ts';
 @Injectable({
@@ -17,20 +12,5 @@ export class GetThemeDataService {
     return API.graphql(
       graphqlOperation(updatedSiteConfiguration)
     ) as Observable<any>;
-
-    /*
-    try {
-      let resultado: Observable<any> = (await API.graphql({
-        ...graphqlOperation(updatedSiteConfiguration),
-        authMode: 'AMAZON_COGNITO_USER_POOLS',
-      })) as Observable<any>;
-      if (resultado) {
-        console.log(resultado);
-      } else {
-        console.log('resultado');
-      }
-    } catch (error) {
-      console.log(error);
-    }*/
   }
 }
