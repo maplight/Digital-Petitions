1bdab350fcf6a5249ebf73785ca4ef59d8d8366a
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-10-19 17:59:11 -0400

    DP-336-Remove unnecessary code

--- src/app/logic/signature/signature.service.spec.ts
+++ src/app/logic/signature/signature.service.spec.ts
@@ -45,35 +45,6 @@ describe('SignatureService', () => {
       done();
     });
   });
-  //getSignatures
-  it('getSignatures should return a SignatureConnection object when the promise it succesful resolve', (done) => {
-    API.graphql = jasmine.createSpy().and.returnValue(
-      new Promise((resolve, reject) => {
-        resolve({ data: _getSignaturesByPetitionQuery });
-      })
-    );
-    service.getSignatures({ petition: '' }).subscribe((data) => {
-      expect(data.result).toEqual({
-        __typename: 'SignatureConnection',
-        items: [],
-        token: undefined,
-      });
-      done();
-    });
-  });
-
-  it('getSignatures should return a error message when the promise it rejected', (done) => {
-    API.graphql = jasmine.createSpy().and.returnValue(
-      new Promise((resolve, reject) => {
-        reject({ errors: [{ message: 'example' }] });
-      })
-    );
-
-    service.getSignatures({ petition: '' }).subscribe((data) => {
-      expect(data.error).toEqual('example');
-      done();
-    });
-  });
 
   //approveSignature
   it('approveSignature should return a string value when the promise it succesful resolve', (done) => {
