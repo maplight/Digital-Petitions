1258581f818c75c491168887254b371ff8042792
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-10-22 09:28:11 -0400

    DP-323-Add unit tests for view-signatures-table component

--- src/app/features/view-signatures/view-signatures-table/view-signatures-table.component.spec.ts
+++ src/app/features/view-signatures/view-signatures-table/view-signatures-table.component.spec.ts
@@ -1,4 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { By } from '@angular/platform-browser';
+import {
+  Signature,
+  SignatureStatus,
+  VerificationMethod,
+} from 'src/app/core/api/API';
 
 import { ViewSignaturesTableComponent } from './view-signatures-table.component';
 
@@ -8,16 +17,96 @@ describe('ViewSignaturesTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewSignaturesTableComponent ]
-    })
-    .compileComponents();
+      declarations: [ViewSignaturesTableComponent],
+      imports: [CommonModule, MatTableModule, MatCheckboxModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ViewSignaturesTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('"elementChecked" function should return true when a unchecked item is clicked', () => {
+    component.dataSource = _signatures;
+    fixture.detectChanges();
+
+    fixture.debugElement
+      .queryAll(By.css('mat-checkbox'))[0]
+      .triggerEventHandler('change');
+
+    expect(component.elementChecked(_signatures[0])).toBeTrue();
+  });
+
+  it('"elementChecked" function should return false when a checked item is clicked', () => {
+    component.dataSource = _signatures;
+    fixture.detectChanges();
+
+    fixture.debugElement
+      .queryAll(By.css('mat-checkbox'))[0]
+      .triggerEventHandler('change');
+
+    expect(component.elementChecked(_signatures[1])).toBeFalse();
+  });
+
+  it('"emitSignaturesSelected" should emit an array of Signatures selected when some table item is clicked and exist selected signatures', (done) => {
+    component.dataSource = _signatures;
+    fixture.detectChanges();
+    component.emitSignaturesSelected.asObservable().subscribe((data) => {
+      expect(data).toEqual([_item]);
+      done();
+    });
+    fixture.debugElement
+      .queryAll(By.css('mat-checkbox'))[0]
+      .triggerEventHandler('change');
+  });
+  it('"emitSignaturesSelected" should emit an empty array when some table item is clicked and not exist selected signatures', (done) => {
+    component.dataSource = _signatures;
+    fixture.detectChanges();
+    component.event(_item);
+    component.emitSignaturesSelected.asObservable().subscribe((data) => {
+      expect(data).toEqual([]);
+      done();
+    });
+
+    fixture.debugElement
+      .queryAll(By.css('mat-checkbox'))[0]
+      .triggerEventHandler('change');
+  });
 });
+const _item: Signature = {
+  __typename: 'Signature',
+  PK: '1',
+  address: '',
+  createdAt: '',
+  isVerified: false,
+  method: VerificationMethod.CALL,
+  name: '',
+  signer: '',
+  status: SignatureStatus.APPROVED,
+  updatedAt: '',
+};
+const _item2: Signature = {
+  __typename: 'Signature',
+  PK: '2',
+  address: '',
+  createdAt: '',
+  isVerified: false,
+  method: VerificationMethod.CALL,
+  name: '',
+  signer: '',
+  status: SignatureStatus.APPROVED,
+  updatedAt: '',
+};
+const _signatures: Signature[] = [
+  _item,
+  _item2,
+  _item2,
+  _item2,
+  _item2,
+  _item2,
+  _item2,
+  _item2,
+];
