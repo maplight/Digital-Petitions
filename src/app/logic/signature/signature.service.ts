d0d0948d2b88037ff3d3974675a700722a79f651
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-10-25 08:38:14 -0400

    DP-227-Add the API call to approve signatures

--- src/app/logic/signature/signature.service.ts
+++ src/app/logic/signature/signature.service.ts
@@ -3,12 +3,17 @@ import { GraphQLResult } from '@aws-amplify/api-graphql';
 import { API } from 'aws-amplify';
 import { catchError, delay, from, map, Observable, of } from 'rxjs';
 import {
+  ApproveSignatureMutation,
   GetSignaturesByPetitionQuery,
+  RejectSignatureMutation,
+  Signature,
   SignatureConnection,
   SignaturesByPetitionInput,
+  TargetSignatureInput,
 } from 'src/app/core/api/API';
 import { FilterData, Result } from 'src/app/shared/models/exports';
 import { SignaturesData } from 'src/app/shared/models/signatures/signatures-data';
+import { approveSignature, rejectSignature } from 'src/graphql/mutations';
 import { getSignaturesByPetition } from 'src/graphql/queries';
 
 @Injectable({
@@ -31,10 +36,17 @@ export class SignatureService {
       catchError((error) => of({ error: error?.errors[0]?.message }))
     );
   }
-  approveSignature(id: string[]): Observable<Result<string>> {
-    return of({
-      result: id.length.toString(),
-    }).pipe(delay(3000));
+  approveSignature(data: TargetSignatureInput): Observable<Result<Signature>> {
+    return from(
+      API.graphql({
+        query: approveSignature,
+        variables: { data: data },
+        authMode: 'AMAZON_COGNITO_USER_POOLS',
+      }) as Promise<GraphQLResult<ApproveSignatureMutation>>
+    ).pipe(
+      map(({ data }) => ({ result: data?.approveSignature as Signature })),
+      catchError((error) => of({ error: error?.errors[0]?.message }))
+    );
   }
   denySignature(id: string[]): Observable<Result<string>> {
     return of({
