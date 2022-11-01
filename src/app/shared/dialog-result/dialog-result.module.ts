a4006a5f7a622e509eae95f18cccd4c79406ab23
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-10-31 20:51:49 -0400

    DP-350-Fix the event of approving petitions

--- src/app/shared/dialog-result/dialog-result.module.ts
+++ src/app/shared/dialog-result/dialog-result.module.ts
@@ -4,10 +4,17 @@ import { DialogResultComponent } from './dialog-result.component';
 import { MatIconModule } from '@angular/material/icon';
 import { BasicModalModule } from 'src/app/shared/basic-modal/basic-modal.module';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [DialogResultComponent],
-  imports: [CommonModule, MatIconModule, BasicModalModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    BasicModalModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
   exports: [DialogResultComponent],
 })
 export class DialogResultModule {}
